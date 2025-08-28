d230d7da6abd2237cb1e188eed2031e1c156168a
sritik97 <ritiksingh9708@gmail.com>
2025-08-28 09:32:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.static(path.join(__dirname, './Mern-portfolio/dist')))
 //routes
 app.use("/api/v1/portfolio", require("./Routes/portfolioRoute"));
 
-app.get('*',function(req,res){
+app.get('/{*any}',function(req,res){
   res.sendFile(path.join(__dirname, './Mern-portfolio/dist/index.html'))
 })
 
